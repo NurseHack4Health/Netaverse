b82539a0f5c768b3ae807ac054041d9170b739d1
Dimuth Kurukula <dimuthkurukula@gmail.com>
2021-11-07 09:00:14 -0500

    init
    create react app, dev env setup with docker

---
+++ web/src/App.tsx
@@ -0,0 +1,29 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { Logo } from "./Logo"
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={8}>
+          <Logo h="40vmin" pointerEvents="none" />
+          <Text>
+		Welcome to Netaverse. Nurse driven education.
+          </Text>
+        </VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
